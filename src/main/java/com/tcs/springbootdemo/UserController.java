77f7c91e801c7591d10ce636f80171012b9a291c
TcsAlisha <tcsalishadias@gmail.com>
2021-08-25 05:18:23 +0000

    put method to update data

--- src/main/java/com/tcs/springbootdemo/UserController.java
+++ src/main/java/com/tcs/springbootdemo/UserController.java
@@ -3,6 +3,7 @@ package com.tcs.springbootdemo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -20,6 +21,13 @@ public class UserController {
 	
 	@PostMapping("/user")
 	public void saveuser(@RequestBody User user) {
+		userservice.save(user);
+		System.out.println(user.getFirstName());
+		
+	}
+	
+	@PutMapping("/user")
+	public void updateUser(@RequestBody User user) {
 		userservice.save(user);
 		System.out.println(user.getFirstName());
 		
